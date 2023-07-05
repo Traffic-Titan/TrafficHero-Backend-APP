7618a5be461621e9b873b08aca0f4482025416c8
ZiJie <zsp97118@gmail.com>
2023-07-06 00:12:37 +0800

    feat: 增加scheduler，以固定時間執行Function

--- News/main.py
+++ News/main.py
@@ -3,58 +3,13 @@
 2. 缺大眾運輸最新消息
 """
 from fastapi import APIRouter
-import os
-import requests
-import json
+from TDX import get_data_response
 
 News_Router = APIRouter(tags=["2.最新消息"],prefix="/News")
 
-def get_data_response(url):
-    auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
-    app_id = os.getenv('TDX_app_id')
-    app_key = os.getenv('TDX_app_key')
-    auth = Auth(app_id, app_key)
-    try:
-        auth_response = requests.post(auth_url, auth.get_auth_header())
-        d = Data(app_id, app_key, auth_response)
-        data_response = requests.get(url, headers=d.get_data_header())
-    except Exception as e:
-        print(f"Error occurred: {str(e)}")
-        return None
-    data_all = json.loads(data_response.text)
-    return data_all
-class Auth():
-    def __init__(self, app_id, app_key):
-        self.app_id = app_id
-        self.app_key = app_key
-
-    def get_auth_header(self):
-        content_type = 'application/x-www-form-urlencoded'
-        grant_type = 'client_credentials'
-
-        return {
-            'content-type': content_type,
-            'grant_type': grant_type,
-            'client_id': self.app_id,
-            'client_secret': self.app_key
-        }
-class Data():
-    def __init__(self, app_id, app_key, auth_response):
-        self.app_id = app_id
-        self.app_key = app_key
-        self.auth_response = auth_response
-
-    def get_data_header(self):
-        auth_JSON = json.loads(self.auth_response.text)
-        access_token = auth_JSON.get('access_token')
-        return {
-            'authorization': 'Bearer '+access_token
-        }
-
-
 """
 1.資料來源:省道最新消息
-    https://tdx.transportdata.tw/api/basic/v2/Road/Traffic/Live/News/Highway?%24top=30&%24format=JSON
+    https://tdx.transportdata.tw/api-service/swagger/basic/7f07d940-91a4-495d-9465-1c9df89d709c#/HighwayTraffic/Live_News_Highway
 """
 @News_Router.get("/provincialWayNews",summary="從TDX上獲取省道最新消息")
 async def provincialWayNews():
@@ -62,7 +17,7 @@ async def provincialWayNews():
     url = "https://tdx.transportdata.tw/api/basic/v2/Road/Traffic/Live/News/Highway?%24top=30&%24format=JSON"
     dataAll = get_data_response(url)
     
-    # allInfo:包全部的資料、 newsArray:包Context(內文)的陣列、 Context:內文，有Title(標題)跟Description(描述)
+    # allInfo:包全部的資料、 newsArray:包Context(內文)的陣列、 Context:內文，有Title(標題)、Description(描述)、UpdateTime(更新時間)
     allInfo = {}
     newsArray = []
     Context= {}
@@ -71,6 +26,7 @@ async def provincialWayNews():
     for info in dataAll['Newses']:
         Context['Title'] = info['Title']
         Context['Description'] = info['Description']
-        newsArray.append({'Title':Context['Title'],'Description':Context['Description']})
+        Context['UpdateTime'] = info['UpdateTime']
+        newsArray.append({'Title':Context['Title'],'Description':Context['Description'],'UpdateTime':Context['UpdateTime']})
     allInfo['Newses'] = newsArray
     return (allInfo)
--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 
 from fastapi import FastAPI
 import os
+import datetime
+import threading
 from dotenv import load_dotenv
-
 from Account.main import Account_Router
 from Smart_Assistant.main import Smart_Assistant_Router
 from Home.main import Home_Router
@@ -15,7 +16,8 @@ from CMS import Speed_Enforcement, Technical_Enforcement,PBS
 from Road_Information.main import Road_Information_Router
 from Tourism_Information.main import Tourism_Information_Router
 from Public_Transport_Information.main import Public_Transport_Information_Router
-
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
 app = FastAPI()
 
 app.include_router(Account_Router)
@@ -30,6 +32,29 @@ app.include_router(Tourism_Information_Router)
 @app.on_event("startup")
 async def startup_event():
     load_dotenv()
-    Speed_Enforcement.getData()
-    Technical_Enforcement.getData()
-    PBS.getData()
+    setInterval(Speed_Enforcement.getData())
+    setInterval(Technical_Enforcement.getData())
+    setInterval(PBS.getData())
+    # Speed_Enforcement.getData()
+    # Technical_Enforcement.getData()
+    # PBS.getData()
+
+#每天0點0分定時執行Function
+def setInterval(function):
+    #現在時間
+    now_time = datetime.datetime.now()
+
+    #明天時間
+    next_time = now_time + datetime.timedelta(days=+1)
+    next_year = next_time.date().year
+    next_month = next_time.date().month
+    next_day = next_time.date().day
+
+    #獲取明天0點0分時間
+    next_time = datetime.datetime.strptime(str(next_year)+"-"+str(next_month)+"-"+str(next_day)+" 00:00:00","%Y-%m-%d %H:%M:%S")
+
+    #獲取距離明天0點0分的時間 , 單位時間"秒"
+    timerStartTime = (next_time - now_time).total_seconds()
+
+    timer = threading.Timer(timerStartTime,function)
+    timer.start()
