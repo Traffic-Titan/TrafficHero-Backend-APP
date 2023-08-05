c852ceffe3fce04462af597a0bf9803565f4032a
ZiJie <zsp97118@gmail.com>
2023-08-05 10:37:01 +0800

    fix: TDX經緯度轉換bug修補

--- APP/CMS/SpeedLimit.py
+++ APP/CMS/SpeedLimit.py
@@ -15,15 +15,13 @@ from Service.MongoDB import connectDB
 """
 
 def ExpressWay():
-    document = []
+    documents = []
     collection = connectDB('TrafficHero','SpeedLimit_In_Each_Place')
     collection.drop()
-    count = 0
-
     with open(r'./APP/CMS/快速公路通車情形與速限統計表.csv',encoding="utf-8") as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            documents = {
+            document = {
                 "公路名稱":row[0],
                 "路段":row[1],
                 "Lat":None,
@@ -31,8 +29,8 @@ def ExpressWay():
                 "速限":row[2],
                 "備註":row[3]
             }
-            document.append(documents)
-    collection.insert_many(document)
+            documents.append(document)
+    collection.insert_many(documents)
 
     for cursor in collection.find({}):
         roadName = cursor['公路名稱'][0:4]
@@ -41,47 +39,39 @@ def ExpressWay():
             endPoint = get_Startpoint_Endpoint(cursor['路段'])[1]
             url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/1/RoadName/{roadName}/{startPoint}/to/{endPoint}?%24top=1&%24format=JSON"
             dataAll = getData(url)
-            time.sleep(2)
-            count+=1
-            print(dataAll)
-            print(count)
+            time.sleep(5)
+            collection.update_one({"路段":row['路段']},{"$set":{"Lat":dataAll[0]['Lat']}})
+            collection.update_one({"路段":row['路段']},{"$set":{"Lng":dataAll[0]['Lon']}})
 
 def FreeWayTunnel():
-    document = []
+    documents = []
     collection = connectDB('TrafficHero','FreeWay_Tunnel')
     collection.drop()
     with open(r'./APP/CMS/國道隧道里程.csv',encoding="utf-8") as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            try:
-                transit2LatLng_url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/1/RoadName/{str(row[0])}/{get_Startpoint_Endpoint(row[3])[0]}/to/{get_Startpoint_Endpoint(row[3])[1]}?%24top=1&%24format=JSON"
-            except:
-                pass
-                continue
-            dataAll = getData(transit2LatLng_url)
-            if(dataAll):
-                document = {
-                    "國道編號":row[0],
-                    "隧道名稱":row[1],
-                    "車行方向":row[2],
-                    "起訖里程":row[3],
-                    "長度公尺":row[4],
-                    "Lat":dataAll[0]['Lat'],
-                    "Lng":dataAll[0]['Lon']
-                }
-            else:
-                print(transit2LatLng_url)
-                documents = {
-                    "國道編號":row[0],
-                    "隧道名稱":row[1],
-                    "車行方向":row[2],
-                    "起訖里程":row[3],
-                    "長度公尺":row[4],
-                    "Lat":None,
-                    "Lng":None
-                }
-            document.append(documents)
-    collection.insert_many(document)
+            document = {
+                "國道編號":row[0],
+                "隧道名稱":row[1],
+                "車行方向":row[2],
+                "起訖里程":row[3],
+                "長度公尺":row[4],
+                "Lat":None,
+                "Lng":None
+            }
+        documents.append(document)
+    collection.insert_many(documents)
+
+    for cursor in collection.find({}):
+        roadName = cursor['國道編號']
+        if(len(get_Startpoint_Endpoint(cursor['起訖里程']))>0):
+            startPoint = get_Startpoint_Endpoint(cursor['起訖里程'])[0]
+            endPoint = get_Startpoint_Endpoint(cursor['起訖里程'])[1]
+            url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/1/RoadName/{roadName}/{startPoint}/to/{endPoint}?%24top=1&%24format=JSON"
+            dataAll = getData(url)
+            time.sleep(5)
+            collection.update_one({"隧道名稱":row['隧道名稱']},{"$set":{"Lat":dataAll[0]['Lat']}})
+            collection.update_one({"隧道名稱":row['隧道名稱']},{"$set":{"Lng":dataAll[0]['Lon']}})
 def get_Startpoint_Endpoint(Context:str):
     return re.findall('\d\w\D\d\d\d|\d\d\w\D\d\d\d|\d\d\d\w\D\d\d\d',Context)
     
