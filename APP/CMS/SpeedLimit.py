8a5bf44164a62a703e5bb72b361dcdf962a351b1
ZiJie <zsp97118@gmail.com>
2023-08-06 21:55:52 +0800

    feat: 國道隧道、快速道路經緯度完善

--- APP/CMS/SpeedLimit.py
+++ APP/CMS/SpeedLimit.py
@@ -24,30 +24,36 @@ def ExpressWay():
             document = {
                 "公路名稱":row[0],
                 "路段":row[1],
-                "Lat":None,
-                "Lng":None,
+                "Lat_Lng":None,
                 "速限":row[2],
                 "備註":row[3]
             }
             documents.append(document)
     collection.insert_many(documents)
-
+    
     for cursor in collection.find({}):
+        Lat_Lng_array = []
         roadName = cursor['公路名稱'][0:4]
         if(len(get_Startpoint_Endpoint(cursor['路段']))>0):
             startPoint = get_Startpoint_Endpoint(cursor['路段'])[0]
             endPoint = get_Startpoint_Endpoint(cursor['路段'])[1]
-            url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/1/RoadName/{roadName}/{startPoint}/to/{endPoint}?%24top=1&%24format=JSON"
+            url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/1/RoadName/{roadName}/{startPoint}/to/{endPoint}?%24format=JSON"
             dataAll = getData(url)
-            time.sleep(5)
-            collection.update_one({"路段":row['路段']},{"$set":{"Lat":dataAll[0]['Lat']}})
-            collection.update_one({"路段":row['路段']},{"$set":{"Lng":dataAll[0]['Lon']}})
+            time.sleep(4)
 
+            #讀取dataAll裡的Lat、Lng，並更新collection的路段
+            for locate in dataAll:
+                locDocument = [
+                   str(locate['Lat']),
+                   str(locate['Lon'])
+                ]
+                Lat_Lng_array.append(locDocument)
+            collection.update_one({"路段":cursor['路段']},{"$set":{"Lat_Lng":Lat_Lng_array}})
 def FreeWayTunnel():
     documents = []
     collection = connectDB('TrafficHero','FreeWay_Tunnel')
     collection.drop()
-    with open(r'./APP/CMS/國道隧道里程.csv',encoding="utf-8") as csv_file:
+    with open(r'./APP/CMS/國道隧道一覽表.csv',encoding="Big5") as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
             document = {
@@ -56,22 +62,31 @@ def FreeWayTunnel():
                 "車行方向":row[2],
                 "起訖里程":row[3],
                 "長度公尺":row[4],
-                "Lat":None,
-                "Lng":None
+                "Lat_Lng":None
             }
-        documents.append(document)
+            documents.append(document)
     collection.insert_many(documents)
 
     for cursor in collection.find({}):
+        Lat_Lng_array = []
         roadName = cursor['國道編號']
         if(len(get_Startpoint_Endpoint(cursor['起訖里程']))>0):
             startPoint = get_Startpoint_Endpoint(cursor['起訖里程'])[0]
             endPoint = get_Startpoint_Endpoint(cursor['起訖里程'])[1]
-            url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/1/RoadName/{roadName}/{startPoint}/to/{endPoint}?%24top=1&%24format=JSON"
+            url = f"https://tdx.transportdata.tw/api/basic/V3/Map/Road/Sign/RoadClass/0/RoadName/{roadName}/{startPoint}/to/{endPoint}?%24format=JSON"
             dataAll = getData(url)
-            time.sleep(5)
-            collection.update_one({"隧道名稱":row['隧道名稱']},{"$set":{"Lat":dataAll[0]['Lat']}})
-            collection.update_one({"隧道名稱":row['隧道名稱']},{"$set":{"Lng":dataAll[0]['Lon']}})
+            time.sleep(4)
+
+            #讀取dataAll裡的Lat、Lng，並更新collection的路段
+            for locate in dataAll:
+                locDocument = [
+                   str(locate['Lat']),
+                   str(locate['Lon'])
+                ]
+                Lat_Lng_array.append(locDocument)
+            collection.update_one({"隧道名稱":cursor['隧道名稱']},{"$set":{"Lat_Lng":Lat_Lng_array}})     
+
+#分析出路段的起始點
 def get_Startpoint_Endpoint(Context:str):
     return re.findall('\d\w\D\d\d\d|\d\d\w\D\d\d\d|\d\d\d\w\D\d\d\d',Context)
     
