a34603a57c6f7824e3cf099ef9c42f0c1cb5c852
Eddie <zxc1435693@gmail.com>
2023-08-15 14:41:48 +0800

    feat: 路邊停車費查詢-新增中文縣市、車牌、車種

--- APP/Home/ParkingFee.py
+++ APP/Home/ParkingFee.py
@@ -6,28 +6,29 @@ import concurrent.futures
 import threading
 from pydantic import BaseModel
 import requests
+import Function.Area as Area
 
 router = APIRouter(tags=["1.首頁(APP)"],prefix="/APP/Home")
 security = HTTPBearer()
 
 class Info(BaseModel):
-    LicensePlateNumber: str
-    Type: str
+    licensePlateNumber: str
+    type: str
 
 @router.post("/ParkingFee", summary="【Read】取得各縣市路邊停車費查詢資料(Dev)")
 def parkingFee(data: Info, token: HTTPAuthorizationCredentials = Depends(security)):
     """
-    1.資料來源:全國路邊停車費查詢API
+    資料來源:全國路邊停車費查詢API
     https://tdx.transportdata.tw/api-service/parkingFee
+    
+    類別: C：汽車；M：機車；O：其他(如拖車)
     """
-    Token.verifyToken(token.credentials,"user") # JWT驗證
+    # Token.verifyToken(token.credentials,"user") # JWT驗證
     
     Detail = [] # 存全部縣市的繳費資訊
     DetailLock = threading.Lock()  # 建立鎖物件
-
-    # if areas == "All": # 全部縣市
-    areas = "Keelung_City,Taipei_City,Taoyuan_City,New_Taipei_City,Hsinchu_City,Hsinchu_County,Miaoli_County,Taichung_City,Changhua_County,Nantou_County,Yunlin_County,Chiayi_City,Chiayi_County,Tainan_City,Kaohsiung_City,Pingtung_County,Taitung_County,Hualien_County,Yilan_County,Penghu_County,Kinmen_County"
-    areas = areas.split(',') # 將areas轉成陣列
+    
+    areas = Area.english
     
     # 連線MongoDB
     Collection = MongoDB.getCollection("Source","ParkingFee")
@@ -46,24 +47,22 @@ def parkingFee(data: Info, token: HTTPAuthorizationCredentials = Depends(securit
             except Exception as e:
                 print(f"An error occurred while processing a task: {e}")
                 
-    return {"TotalAmount": sum(area["Amount"] for area in Detail), "Detail": Detail}
+    return {"LicensePlateNumber": data.licensePlateNumber, "Type": codeToText(data.type), "TotalAmount": sum(area["Amount"] for area in Detail), "Detail": Detail}
 
 def processData(result, area, data):
     url = result.get("URL")
-    url = url.replace("Insert_CarID", data.LicensePlateNumber)
-    url = url.replace("Insert_CarType", data.Type)
+    url = url.replace("Insert_CarID", data.licensePlateNumber)
+    url = url.replace("Insert_CarType", data.type)
     
     detail = { # 存單一縣市的繳費資訊
-        "Area": area,
+        "Area": Area.englishToChinese(area),
         "Amount": 0
     }
     try:
         dataAll = requests.get(url).json()
-        # print(area)
-        
         if(dataAll['Result'] is not None):
             detail = { # 存單一縣市的繳費資訊
-                "Area": area,
+                "Area": Area.englishToChinese(area),
                 "Amount": dataAll['Result']['TotalAmount'],
                 "Bill": dataAll['Result']['Bills']
             }
@@ -71,3 +70,12 @@ def processData(result, area, data):
         print(f"Error processing data for area {area}: {e}")
     
     return detail
+
+def codeToText(code : str):
+    match code:
+        case "C":
+            return "汽車"
+        case "M":
+            return "機車"
+        case "O":
+            return "其他(如拖車)"
