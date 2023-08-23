27153cd5fb7d988d33020a481073667fc51c2a2e
Eddie <zxc1435693@gmail.com>
2023-08-23 11:32:11 +0800

    feat: 新增取用API Key

---
+++ Universal/API.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import Service.Token as Token
+from dotenv import load_dotenv # 載入.env環境變數檔案
+import os
+
+router = APIRouter(tags=["通用功能"],prefix="/Universal/API")
+security = HTTPBearer()
+
+@router.get("/GoogleMaps",summary="【Read】取得Google Maps API KEY")
+async def getGoogleMaps(token: HTTPAuthorizationCredentials = Depends(security)):
+    Token.verifyClient(token.credentials) # 驗證Token是否來自於官方APP與Website
+    
+    load_dotenv()
+    return os.getenv("Google_Maps_Key")
