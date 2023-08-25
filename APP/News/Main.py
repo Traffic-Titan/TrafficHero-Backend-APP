f1e54f9d91aa6fb6648745992efe4a9fb92a7809
ZiJie <zsp97118@gmail.com>
2023-08-26 07:30:42 +0800

    feat: Youbike、1968最新消息修改

--- APP/News/Main.py
+++ APP/News/Main.py
@@ -18,9 +18,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import urllib.request
-import requests
-import time
-import datetime
 import Function.Link as Link
 import timeit
 import Function.Area as Area
@@ -30,7 +27,7 @@ security = HTTPBearer()
 
 
 @router.get("/PublicTransport/YouBike/{county}",summary="【Read】最新消息-大眾運輸-腳踏車(Dev)")
-async def youbike(county:str, token: HTTPAuthorizationCredentials = Depends(security)):
+async def youbike(country:str, token: HTTPAuthorizationCredentials = Depends(security)):
 
     """
     縣市列表：臺北市、新北市、桃園市、新竹縣、新竹市、新竹科學園區、苗栗縣、台中市、嘉義市、臺南市、高雄市、屏東縣
@@ -43,40 +40,24 @@ async def youbike(county:str, token: HTTPAuthorizationCredentials = Depends(secu
     context = {}
     context_return = []
 
-    #Python Selenium 
-    chrome_options = Options()
-    service = Service()
-    chrome_options.add_argument("--headless")
-    browser = webdriver.Chrome(service=service, options=chrome_options)
-    browser.get("https://www.youbike.com.tw/region/main/news/status/")
-
     # BeautifulSoup4
     response = urllib.request.urlopen('https://www.youbike.com.tw/region/main/news/status/')
     soup = BeautifulSoup(response.read().decode('utf-8'),'html.parser')
+    
+    # 定位Select標籤
+    selectList = soup.find('select',class_='f2e-cursor-pointer')
+    optionList = selectList.find_all('option')
+    for area in optionList:
+        if(area.text == country):
 
+            # 取得各縣市的value. ex: 高雄 -> 12、嘉義 -> 08
+            selectedValue = area.get('value')
     
-    pageNum = soup.find_all('a',class_='cdp_i css-4g6ai3') #定位下排頁面按鈕
-    allDate = soup.find_all('span',class_='news-list-date') #定位發布日期
-    
-    totalPage = 0 # totalPage： 總頁數
-    for page in pageNum:
-        if(int(page.text)>totalPage):
-            totalPage = int(page.text)
-
-    Index = 1
-    for date in allDate:
-        publicDate = datetime.datetime.strptime(date.text, "%Y-%m-%d") #將讀到的事件日期轉成YY-MM-DD HH:MM:SS格式
-        
-        Title = browser.find_element(By.XPATH,'//*[@id="MainContent"]/div[2]/div[3]/div[2]/ul/li['+str(Index)+']/a/span[2]') #符合日期的標題
-        Url = browser.find_element(By.XPATH,'//*[@id="MainContent"]/div[2]/div[3]/div[2]/ul/li['+str(Index)+']/a') #符合日期的URL
-        context = {"標題":Title.text,"URL":Url.get_attribute('href'),"發佈日期":publicDate}
-        context_return.append(context)
-        Index += 1
-    
-    return context_return
+            # 回傳對應縣市的結果
+            return(processData_for_Youbike(soup))
    
 @router.get("/Car/FreeWay",summary="【Read】最新消息-汽車-國道最新消息(Dev)")
-async def freeWay(Date:int,token: HTTPAuthorizationCredentials = Depends(security)):
+async def freeWay(token: HTTPAuthorizationCredentials = Depends(security)):
     """
     國道最新消息：https://1968.freeway.gov.tw/n_whatsup
 
@@ -84,42 +65,82 @@ async def freeWay(Date:int,token: HTTPAuthorizationCredentials = Depends(securit
     """
     # Token.verifyToken(token.credentials,"user") # JWT驗證
 
-    #Python Selenium 
-    chrome_options = Options()
-    service = Service()
-    chrome_options.add_argument("--headless")
-    browser = webdriver.Chrome(service=service, options=chrome_options)
-    browser.get("https://1968.freeway.gov.tw/n_whatsup")
-
-    #  Initial
-    context_return = []
-    context = {}
+    # BeautifulSoup4
+    response_page1 = urllib.request.urlopen('https://1968.freeway.gov.tw/n_whatsup?page=1')
+    soup1 = BeautifulSoup(response_page1.read().decode('utf-8'),'html.parser')
+    response_page2 = urllib.request.urlopen('https://1968.freeway.gov.tw/n_whatsup?page=2')
+    soup2 = BeautifulSoup(response_page2.read().decode('utf-8'),'html.parser')
     
-    dataCount = browser.find_element(By.XPATH,'//*[@id="idScrollTarget"]/div/div[3]/div[2]/div/span[1]') #取得總筆數
-    dateSpecify = (datetime.datetime.now()+datetime.timedelta(days=-Date)) #取得 指定查看幾天前的消息的日期
+    return(processData_for_Freeway(soup1),processData_for_Freeway(soup2))
+    
+# 處理bs4 response(Freeway)後的頁面
+def processData_for_Freeway(soup):
+    
+    #  Initial
+    title_array = []
+    url_array = []
+    publicTime_array = []
+    type_array = []
+    documents = []
 
-    #網站上每五筆顯示一頁，pageNum：存總共有幾頁
-    if(int(dataCount.text)%5 !=0):
-        pageNum = int(int(dataCount.text)/5) + 1
-    else:
-        pageNum = int(int(dataCount.text)/5)
-       
-    for count in range(0,pageNum):
-        Index = 1 #定位該頁的第幾筆資料，預設從 1 開始
-        all_publicDate = browser.find_elements(By.CLASS_NAME,'wup_time') #定位所有發布時間，格式 YYYY-MM-DD HH:mm:ss
 
-        for date in all_publicDate:
-            publicDate = datetime.datetime.strptime(date.text, "%Y-%m-%d %H:%M:%S") #將讀到的事件日期轉成YY-MM-DD HH:MM:SS格式
-            
-            #日期比較
-            if(publicDate > dateSpecify or publicDate == dateSpecify):
-                Title = browser.find_element(By.XPATH,'//*[@id="idScrollTarget"]/div/div[3]/div[3]/div['+str(Index)+']/div[1]/span') #符合日期的標題
-                Url = browser.find_element(By.XPATH,'//*[@id="idScrollTarget"]/div/div[3]/div[3]/div['+str(Index)+']/div[2]/span/a') #符合日期的URL
-                Type = browser.find_element(By.XPATH,'//*[@id="idScrollTarget"]/div/div[3]/div[3]/div['+str(Index)+']/div[1]/div/span[1]') #符合日期的Type
-                context = {"標題":Title.text,"URL":Url.get_attribute('href'),"發佈日期":publicDate,"類型":Type.text}
-                context_return.append(context)
-                Index += 1
+    # Title處理
+    all_title = soup.find_all('span',class_="wup_title_txt use_tri_icon")
+    for title in all_title:
+        title_array.append(title.text)
 
-        browser.find_element(By.XPATH,'//*[@id="idScrollTarget"]/div/div[3]/div[4]/div/a['+str(pageNum + 1)+']').click()# 點擊下一頁按鈕定位
-        time.sleep(1)
-    return context_return
+    # url處理
+    all_url = soup.find_all('span',class_="wup_seemore")
+    for url in all_url:
+        url_array.append(url.find('a').get('href'))
+
+    # publicTime處理
+    all_publicTime = soup.find_all('span',class_="wup_time")
+    for time in all_publicTime:
+        publicTime_array.append(time.text)
+
+    # type處理
+    all_type = soup.find_all('span',attrs={"class":["wup_type tp_cons","wup_type tp_rinfo"]})
+    for type in  all_type:
+        type_array.append(type.text.strip())
+    
+    for data in range(0,len(all_title)):
+        document = {
+            "標題":title_array[data],
+            "發佈時間":publicTime_array[data],
+            "發佈類型":type_array[data],
+            "URL":url_array[data]
+        }
+        documents.append(document)
+    return documents
+# 處理bs4 response(Youbike)後的頁面
+def processData_for_Youbike(soup):
+    #  Initial
+    title_array = []
+    url_array = []
+    publicTime_array = []
+    documents = []
+
+
+    # Title處理
+    all_title = soup.find_all('span',class_="news-list-type")
+    for title in all_title:
+        title_array.append(title.text)
+
+    # publicTime處理
+    all_publicTime = soup.find_all('span',class_="news-list-date")
+    for time in all_publicTime:
+        publicTime_array.append(time.text)
+
+        # url處理，url是 class:news-list-date 的父類
+        url = time.findParent('a')
+        url_array.append(url.get('href'))
+    
+    for data in range(0,len(all_title)):
+        document = {
+            "標題":title_array[data],
+            "發佈時間":publicTime_array[data],
+            "URL":url_array[data]
+        }
+        documents.append(document)
+    return documents
