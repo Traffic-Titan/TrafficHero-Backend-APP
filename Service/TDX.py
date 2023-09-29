419e5fc2fa4b7a7d54b1775c348a409039dda006
Eddie <zxc1435693@gmail.com>
2023-09-29 18:01:00 +0800

    fix: 新增Token判斷功能，以確保可以正常call TDX API

--- Service/TDX.py
+++ Service/TDX.py
@@ -5,8 +5,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import Service.Token as Token
 import Function.Time as Time
+import time
 
 router = APIRouter(tags=["外部服務(Dev Only)"],prefix="/Service/TDX")
+
+# global count # Dev
+# count = 0 # Dev
 
 @router.get("/getData", summary="TDX - 取得資料")
 async def getDataAPI(url:str, token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
@@ -14,13 +18,18 @@ async def getDataAPI(url:str, token: HTTPAuthorizationCredentials = Depends(HTTP
     return getData(url)
 
 def getData(url):
+    # global count # Dev
+    # count += 1 # Dev
+    # print(f"count: {count}") # Dev
     auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
     app_id = os.getenv('TDX_app_id')
     app_key = os.getenv('TDX_app_key')
     auth = Auth(app_id, app_key)
     try:
         auth_response = requests.post(auth_url, auth.get_auth_header())
-        d = Data(app_id, app_key, auth_response)
+        while auth_response.status_code != 200: # 確認是否取得token，以確保可以正常call TDX API
+            auth_response = requests.post(auth_url, auth.get_auth_header())
+        d = data(app_id, app_key, auth_response)
         data_response = requests.get(url, headers=d.get_data_header())
     except Exception as e:
         print(f"Error occurred: {str(e)}")
@@ -43,7 +52,7 @@ class Auth():
             'client_secret': self.app_key
         }
 
-class Data():
+class data():
     def __init__(self, app_id, app_key, auth_response):
         self.app_id = app_id
         self.app_key = app_key
@@ -52,8 +61,9 @@ class Data():
     def get_data_header(self):
         auth_JSON = json.loads(self.auth_response.text)
         access_token = auth_JSON.get('access_token')
-        return {
-            'authorization': 'Bearer '+ access_token
+
+        return{
+            'authorization': 'Bearer ' + access_token
         }
 
 @router.get("/getHealthStatus", summary="TDX - 取得服務健康狀態(Dev)")
